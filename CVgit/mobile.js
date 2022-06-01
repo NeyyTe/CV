d72635b7a5ee633b161e6975be11b84817e82546
NeyyTe <marechal.maxxence@outlook.fr>
2022-06-01 14:51:12 +0200

    CV

--- CVgit/mobile.js
+++ CVgit/mobile.js
@@ -1,10 +1,12 @@
+
 if (window.matchMedia("(max-width: 858px)").matches) {
     var lesclasseasupprimer = document.getElementsByClassName('btn1');
     Array.from(lesclasseasupprimer).forEach((laclasseasupprimer, index) => {
         laclasseasupprimer.classList.remove('btn1');
         }
     );
-} else {
+}
+ else {
     /* the view port is max 858 pixels wide */
     console.log("Grand ecran");
 }
